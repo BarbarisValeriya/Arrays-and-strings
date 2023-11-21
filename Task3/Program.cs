1d326aa94ae5723d61e7d54b2261b78e11164eb0
Valeriia <borisova.valeriya.n@mail.ru>
2023-11-21 20:00:15 +0300

    палиндром

---
+++ Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+Console.WriteLine ("введите текст");
+string text = Console.ReadLine();
+text = text.Replace(" ", "");
+
+string Palindrom(string text)
+{
+    char[] OBRtext = text.ToCharArray();
+    Array.Reverse(OBRtext);
+    string finaltext = new string(OBRtext);
+    if (text == finaltext)
+    {
+     Console.WriteLine("Данная запись является палиндромом");
+    }
+    else
+    {
+     Console.WriteLine("Данная запись не является палиндромом");
+    }
+    return text;
+}
+
+Console.WriteLine(Palindrom(text));
+
+
+
+
+
