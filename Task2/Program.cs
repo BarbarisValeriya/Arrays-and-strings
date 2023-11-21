3a9683cccec13cc6decccd224c535ead14efa270
Valeriia <borisova.valeriya.n@mail.ru>
2023-11-21 19:31:43 +0300

    с туловер вполне легко

---
+++ Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+Console.WriteLine ("введите текст");
+string text = Console.ReadLine();
+text = text.ToLower();
+Console.WriteLine("Полученная строка " + text);
+Console.ReadKey();
+
+
