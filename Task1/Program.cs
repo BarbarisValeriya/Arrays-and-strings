63c95b3e5a4e853000cf100b0466927881c3e07f
Valeriia <borisova.valeriya.n@mail.ru>
2023-11-21 19:25:00 +0300

    решено, но строка слишком длинная с учтом всех переборов

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,3 +1,37 @@
 ﻿// Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
 
+char[,] matrix = new char[,]
+{
+    {'-', 'b', 'c', 'd', 'e'},
+    {'r', 't', '-', 'u', 'i'},
+    {'w', '-', 'q', '!', 'm'},
+};
+
+void PrintMatrix(char[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintMatrix(matrix);
+
+string MatrixString (char[,] matrix)
+{
+    string result = "";
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+       for (int j = 0; j < matrix.GetLength(1); j++)
+       {
+        result = result + matrix[i,j] + result;
+       }
+    }
+    return result;
+}
+Console.WriteLine(MatrixString(matrix));
 
